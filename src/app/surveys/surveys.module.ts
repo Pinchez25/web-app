7cb2b9b955566e752a7868bde20bbadb49af7c78
Pinchez25 <thuamwangi001@gmail.com>
2025-04-09 09:48:53 +0300

    Implement SurveysModule with routing for survey statistics

---
+++ src/app/surveys/surveys.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SurveyStatisticsComponent } from './survey-statistics/survey-statistics.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { Route } from '../core/route/route.service';
+
+const routes: Routes = [
+  Route.withShell([
+    {
+      path: 'surveys',
+      data: { title: 'Surveys', breadcrumb: 'Surveys' },
+      children: [
+        {
+          path: 'statistics',
+          component: SurveyStatisticsComponent,
+          data: { title: 'Survey Statistics', breadcrumb: 'Statistics' }
+        }
+      ]
+    }
+  ])
+
+];
+
+@NgModule({
+  declarations: [
+    SurveyStatisticsComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)]
+})
+export class SurveysModule {}
